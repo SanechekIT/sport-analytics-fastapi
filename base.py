8237d957e11fa9a6175dbd32526370997f19694c
Ваше Имя <-email@example.com>
2026-01-18 21:20:42 +0300

    Initial commit

--- base.py
+++ base.py
@@ -1,7 +1,6 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime
 from typing import Optional
-11111111111111111111111111111111111111111111
 
 class BaseModel(SQLModel):
     id: Optional[int] = Field(default=None, primary_key=True)
