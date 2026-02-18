42db471e627941ff85cda8098ceb226367115d22
SanechekIT <xxxsashulaxxx@gmail.com>
2026-02-18 08:47:30 +0300

    Update exercise.py

--- app/models/exercise.py
+++ app/models/exercise.py
@@ -5,7 +5,7 @@ class Exercise(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     workout_id: int = Field(foreign_key="workout.id")
-    description: Optional[str] = Field(default=None)  # сделаем опциональным
+    description: Optional[str] = Field(default=None)  
     sets: int
     reps: int
     weight: Optional[int] = Field(default=None)  # вес может быть None (упражнения с весом тела)
