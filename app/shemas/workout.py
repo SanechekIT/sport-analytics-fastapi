d00d930617d1e606455a58e9ef09eb4aa16bc9cb
SanechekIT <xxxsashulaxxx@gmail.com>
2026-02-20 11:32:40 +0300

    Update workout.py

--- app/shemas/workout.py
+++ app/shemas/workout.py
@@ -2,16 +2,18 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
 
+# Базовый класс - только общие поля
+class WorkoutBase(SQLModel):  # Наследуем от SQLModel, но НЕ table=True
+    name: str
+    date: datetime  # date это дата/время, поэтому datetime, а не int
+    duration_minutes: int
+    notes: str
 
-class WorkoutBase(SQLModel):
-    name:str
-    date:datetime
-    duration_minutes:int
-    notes:str
-
+# Для создания - все поля из Base (обязательные)
 class WorkoutCreate(WorkoutBase):
-    pass #Пустой,тк наследует все поля из Base
+    pass  # Пустой, потому что берет все поля из Base
 
-class Workout(WorkoutBase,table = True):
+# Для чтения/работы с БД - Base + служебные поля
+class Workout(WorkoutBase, table=True):  # table=True - это таблица в БД
     id: Optional[int] = Field(default=None, primary_key=True)
-    user_id: int = Field(fpreign_key = "user.id")
+    user_id: int = Field(foreign_key="user.id")
