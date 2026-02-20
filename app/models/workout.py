2bb9b55a9def2902339b950048c4bd9c15ecd632
SanechekIT <xxxsashulaxxx@gmail.com>
2026-02-20 10:41:24 +0300

    Update workout.py

--- app/models/workout.py
+++ app/models/workout.py
@@ -4,7 +4,9 @@ from datetime import datetime
 
 class Workout(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
     title: str
     description: Optional[str] = None
     date: Optional[datetime] = None  # или datetime = Field(default_factory=datetime.now)
     user_id: int = Field(foreign_key="user.id")
+    notes: str
