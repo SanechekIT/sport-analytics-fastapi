d24a0dc63de42d2641ee9c97f65839a0db4230af
SanechekIT <xxxsashulaxxx@gmail.com>
2026-02-20 11:29:22 +0300

    Update workout.py

--- app/shemas/workout.py
+++ app/shemas/workout.py
@@ -1,1 +1,17 @@
- 
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from datetime import datetime
+
+
+class WorkoutBase(SQLModel):
+    name:str
+    date:datetime
+    duration_minutes:int
+    notes:str
+
+class WorkoutCreate(WorkoutBase):
+    pass #Пустой,тк наследует все поля из Base
+
+class Workout(WorkoutBase,table = True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(fpreign_key = "user.id")
