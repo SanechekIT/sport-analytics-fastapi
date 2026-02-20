27a6d0ca47d30f9923bb322f2f18fd4bb155a98a
SanechekIT <xxxsashulaxxx@gmail.com>
2026-02-20 23:29:11 +0300

    Update workout.py

--- app/models/workout.py
+++ app/models/workout.py
@@ -1,6 +1,7 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
+from sqlmodel import SQLModel, Field, Relationship
 
 class Workout(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -10,3 +11,6 @@ class Workout(SQLModel, table=True):
     date: Optional[datetime] = None  # или datetime = Field(default_factory=datetime.now)
     user_id: int = Field(foreign_key="user.id")
     notes: str
+
+    # Добавил relationship:
+    exercises: List["WorkoutExercise"] = Relationship(back_populates="workout")
