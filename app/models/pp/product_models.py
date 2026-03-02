11c26d25e9ef3cb32d6259151e86eea439b9917a
SanechekIT <xxxsashulaxxx@gmail.com>
2026-03-02 16:18:46 +0300

    Update product_models.py

--- app/models/pp/product_models.py
+++ app/models/pp/product_models.py
@@ -1,1 +1,13 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from datetime import datetime
 
+class Product(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    calories: Optional[int] = None
+    proteins: Optional[float] = None
+    fats: Optional[float] = None
+    carbs: Optional[float] = None
+    category: Optional[str] = None
+    created_at: datetime = Field(default_factory=datetime.now)
