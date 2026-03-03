d26ff76aefac32ee8cac470c53bca78d6a0b8f87
SanechekIT <xxxsashulaxxx@gmail.com>
2026-03-03 18:57:17 +0300

    Update mealItem.py

--- app/models/mealItem.py
+++ app/models/mealItem.py
@@ -1,1 +1,13 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from datetime import datetime
+
+
+class Mealitem(SQLMeal):
+    __tablenames__ = "mealitem"
+
+    id: Optional[int] = Field(default = None,primary_key=True)
+    meal_id: int = Field(foreign_key="meal.id", index=True)
+    product_id: int = Field(default=None,primary_key=True)
+    grams: float = Field(gt=0) 
 
