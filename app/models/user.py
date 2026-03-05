567982a79c04ca5db7cc70c32555795d2ecc79ac
SanechekIT <xxxsashulaxxx@gmail.com>
2026-03-05 14:15:48 +0300

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,15 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List, TYPE_CHECKING
 
-class User(SQLModel, table=True, extend_existing=True):
+if TYPE_CHECKING:
+    from app.models.meal import Meal
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
     id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True)
+    meals: List["Meal"] = Relationship(back_populates="user")
     email: str = Field(unique=True, index=True)
     hashed_password: str
