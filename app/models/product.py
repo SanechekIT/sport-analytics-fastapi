50a047b65aa09793c897aeb602712f864df626d3
SanechekIT <xxxsashulaxxx@gmail.com>
2026-03-05 14:15:24 +0300

    Update product.py

--- app/models/product.py
+++ app/models/product.py
@@ -1,10 +1,20 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List, TYPE_CHECKING
 
-class Product(SQLModel,table=True):
-    __tablename__ = "products" 
-    
+if TYPE_CHECKING:
+    from app.models.meal_item import MealItem
+
+
+class Product(SQLModel, table=True):
+    __tablename__ = "products"
+
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str = Field(max_length=200, index=True)
-    calories_per_100g: Optional[int] = Field(default=None, ge=0)
-   
+    name: str = Field(index=True, unique=True)
+    calories: float
+    proteins: float
+    fats: float
+    carbs: float
+    is_available: bool = Field(default=True)
+
+    # ДОБАВЛЕНО: связь с MealItem
+    meal_items: List["MealItem"] = Relationship(back_populates="product")
