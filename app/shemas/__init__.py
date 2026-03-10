c684cdefbba59fcc821971a75fd9b1b7642489da
SanechekIT <xxxsashulaxxx@gmail.com>
2026-03-11 00:05:00 +0300

    Update __init__.py

--- app/shemas/__init__.py
+++ app/shemas/__init__.py
@@ -1,1 +1,4 @@
- 
+from .user import User, UserCreate, UserUpdate
+from .workout import Workout, WorkoutCreate, WorkoutUpdate
+from .exercise import ExercisePublic as Exercise, ExerciseCreate, ExerciseUpdate
+from .meal_item import MealItem, MealItemCreate, MealItemUpdate
