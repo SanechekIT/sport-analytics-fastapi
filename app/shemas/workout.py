014dd449d715cd152f0864e3ffa20c170d64c62f
SanechekIT <xxxsashulaxxx@gmail.com>
2026-03-11 00:06:29 +0300

    Update workout.py

--- app/shemas/workout.py
+++ app/shemas/workout.py
@@ -8,30 +8,17 @@ class WorkoutBase(BaseModel):
     duration: int  # в минутах
     calories_burned: int
 
-
 class Workout(WorkoutBase):
     id: int
     created_at: datetime
-    user_id: int  # если привязано к пользователю
+    user_id: int
 
     class Config:
         from_attributes = True
-        
+
 # Схема для создания тренировки (то, что приходит в запросе)
 class WorkoutCreate(BaseModel):
     name: str
     description: Optional[str] = None
     date: Optional[datetime] = None
     duration: Optional[int] = None
-
-# Схема для ответа с данными тренировки
-class WorkoutResponse(BaseModel):
-    id: int
-    user_id: int
-    name: str
-    description: Optional[str] = None
-    date: datetime
-    duration: Optional[int] = None
-    
-    class Config:
-        from_attributes = True
