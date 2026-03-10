9b5377c48fc1e1b4fee195af4eaba4b033296872
SanechekIT <xxxsashulaxxx@gmail.com>
2026-03-11 00:03:02 +0300

    Update workout.py

--- app/shemas/workout.py
+++ app/shemas/workout.py
@@ -2,6 +2,21 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
+class WorkoutBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+    duration: int  # в минутах
+    calories_burned: int
+
+
+class Workout(WorkoutBase):
+    id: int
+    created_at: datetime
+    user_id: int  # если привязано к пользователю
+
+    class Config:
+        from_attributes = True
+        
 # Схема для создания тренировки (то, что приходит в запросе)
 class WorkoutCreate(BaseModel):
     name: str
