17a77598046c69830f2eef31ff4d853dc392ef7e
SanechekIT <xxxsashulaxxx@gmail.com>
2026-06-10 10:10:04 +0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from .user import User
 from .product import Product
 from .meal import Meal
-from .mealitem import MealItem
+from .mealItem import MealItem
 
 __all__ = ["User","Meal", "Product", "MealItem"]
